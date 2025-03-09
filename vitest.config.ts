ad0bae84e810724ecab33e355fbf2dd9a3690cac
Mindaugas Kristutis <mindaugas@devrunners.com>
2025-03-09 12:45:54 +0200

    feat: add path alias configuration and update module resolution settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    test: {
-        include: ['tests/**/*.test.{ts,js}'],
-        setupFiles: ['./tests/setup.ts'],
+  test: {
+    include: ['tests/**/*.test.{ts,js}'],
+    setupFiles: ['./tests/setup.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
     },
+  },
 })
